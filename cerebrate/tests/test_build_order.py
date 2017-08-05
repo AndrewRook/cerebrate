c3d553ad233adcc185c38049ae0222b82f577eac
Andrew Schechtman-Rook <rook166@gmail.com>
2017-08-04 21:34:13 -0400

    more test boilerplate

--- cerebrate/tests/test_build_order.py
+++ cerebrate/tests/test_build_order.py
@@ -22,10 +22,18 @@ class TestRandomlyChooseNextBuildItem(object):
         required = defaultdict(int, {pt.dragoon: 4})
         choice = bo.randomly_choose_next_build_item(existing, required, pt.builder)
         assert choice is pt.cybernetics_core
-    @pytest.mark.skip(reason="TODO: code up this logic")
+    @pytest.mark.skip(reason="TODO (AndrewRook): code up this logic in the function itself")
     def test_consume_met(self):
         pass
-    @pytest.mark.skip(reason="TODO: code up this logic")
+    @pytest.mark.skip(reason="TODO (AndrewRook): code up this logic in the function itself")
     def test_consume_not_met(self):
         pass
+    @pytest.mark.skip(reason="TODO (AndrewRook): it was late and I was tired")
+    def test_existing_prereq_set_to_zero(self):
+        pass
+    @pytest.mark.skip(reason="TODO (AndrewRook): it was late and I was tired")
+    def test_required_prereq_set_to_zero(self):
+        pass
+    
+    
     
