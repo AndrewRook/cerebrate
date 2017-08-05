7bf4c5ed1e090ae1fe03263d93cc6b56f9d0d586
Andrew Schechtman-Rook <rook166@gmail.com>
2017-08-04 21:32:21 -0400

    added some boilerplate for future tests

--- cerebrate/tests/test_build_order.py
+++ cerebrate/tests/test_build_order.py
@@ -22,4 +22,10 @@ class TestRandomlyChooseNextBuildItem(object):
         required = defaultdict(int, {pt.dragoon: 4})
         choice = bo.randomly_choose_next_build_item(existing, required, pt.builder)
         assert choice is pt.cybernetics_core
-        
+    @pytest.mark.skip(reason="TODO: code up this logic")
+    def test_consume_met(self):
+        pass
+    @pytest.mark.skip(reason="TODO: code up this logic")
+    def test_consume_not_met(self):
+        pass
+    
